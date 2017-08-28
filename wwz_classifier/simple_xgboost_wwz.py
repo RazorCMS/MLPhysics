5566b940163046cf6020bdb8de0404d43f759c4c
Cristián H. Pena <cristian.pena@caltech.edu>
2017-08-28 15:33:42 -0700

    Adding histogram for bkg and signal discriminator

--- wwz_classifier/simple_xgboost_wwz.py
+++ wwz_classifier/simple_xgboost_wwz.py
@@ -6,10 +6,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, roc_curve
 import root_pandas as rp
 import numpy as np
+import pandas as pd
 
 
 ##Define variables to be used
-variables = ['MET','METPhi','lep1Pt','lep2Pt','lep3Pt','lep4Pt','NJet20','NJet30','NBJet20','NBJet30','lep1Phi','lep2Phi','lep3Phi','lep4Phi','lep1Eta','lep2Eta','lep3Eta','lep4Eta','ZMass','ZPt','lep3MT','lep4MT','lep34MT','phi0','theta0','phi','theta1','theta2','phiH','minDRJetToLep3','minDRJetToLep4'] 
+#variables = ['MET','METPhi','lep1Pt','lep2Pt','lep3Pt','lep4Pt','NJet20','NJet30','NBJet20','NBJet30','lep1Phi','lep2Phi','lep3Phi','lep4Phi','lep1Eta','lep2Eta','lep3Eta','lep4Eta','ZMass','ZPt','lep3MT','lep4MT','lep34MT','phi0','theta0','phi','theta1','theta2','phiH','minDRJetToLep3','minDRJetToLep4']
+
+variables = ['MET','METPhi','lep1Pt']
+
 ##Getting ROOT files into pandas
 #df_signal = rp.read_root('/Users/cmorgoth/Work/data/WWZanalysis/MC/WWZAnalysis_WWZJetsTo4L2Nu_4f_TuneCUETP8M1_13TeV_aMCatNLOFxFx_pythia8.root', 'WWZAnalysis', columns=['MET','lep1Pt','lep2Pt','lep3Pt','lep4Pt'])
 #df_bkg = rp.read_root('/Users/cmorgoth/Work/data/WWZanalysis/MC/WWZAnalysis_ZZTo4L_13TeV-amcatnloFXFX-pythia8.root', 'WWZAnalysis', columns=['MET','lep1Pt','lep2Pt','lep3Pt','lep4Pt'])
@@ -61,6 +65,9 @@ print model
 #y_pred = model.predict(x_test)
 y_pred = model.predict_proba(x_test)[:, 1]
 predictions = [round(value) for value in y_pred]
+
+# make histogram of discriminator value for signal and bkg
+pd.DataFrame({'truth':y_test, 'disc':y_pred}).hist(column='disc', by='truth', bins=50)
 
 print y_pred
 
