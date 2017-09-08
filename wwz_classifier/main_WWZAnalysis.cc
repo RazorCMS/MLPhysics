327d7350213cc1ff5de086c7c5555166a568bef1
Cristián H. Pena <cristian.pena@caltech.edu>
2017-09-07 17:11:35 -0700

    updating wwz root analysis

---
+++ wwz_classifier/main_WWZAnalysis.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "WWZAnalysis.hh"
+
+int main()
+{
+
+  TFile* fin = new TFile("/Users/cmorgoth/Work/data/WWZanalysis/MC/WWZAnalysis_WWZJetsTo4L2Nu_4f_TuneCUETP8M1_13TeV_aMCatNLOFxFx_pythia8_1pb_weighted.root", "READ");
+  //  TFile* fin = new TFile("/Users/cmorgoth/Work/data/WWZanalysis/MC/WWZAnalysis_ZZTo4L_13TeV-amcatnloFXFX-pythia8_1pb_weighted.root", "READ");
+  TTree* tree_signal = (TTree*)fin->Get("WWZAnalysis");
+  
+  WWZAnalysis* wwz_signal = new WWZAnalysis( tree_signal );
+  wwz_signal->Loop();
+
+
+  //For Bkg
+  fin = new TFile("/Users/cmorgoth/Work/data/WWZanalysis/MC/WWZAnalysis_ZZTo4L_13TeV-amcatnloFXFX-pythia8_1pb_weighted.root", "READ");
+  TTree* tree_bkg = (TTree*)fin->Get("WWZAnalysis");
+
+  WWZAnalysis* wwz_bkg = new WWZAnalysis( tree_bkg );
+  wwz_bkg->Loop();
+  
+  return 0;
+}
