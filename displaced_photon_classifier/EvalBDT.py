55c3479cab8ec26cc66f4460fa6b00ccf7fcb041
Dustin Anderson <dustin.james.anderson@cern.ch>
2018-04-19 03:55:06 +0200

    Add script to run BDT on batches of events

---
+++ displaced_photon_classifier/EvalBDT.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+import os
+import math
+import argparse
+import itertools
+import cPickle as pickle
+
+import numpy as np
+import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_curve
+import ROOT as root
+import root_pandas as rp
+
+def get_disc_or_minusone(event, disc_lookup):
+    """
+    Checks the event ID (run/lumi/event) from the tree (first argument)
+    and gets the discriminator value corresponding to the event, 
+    if available in the lookup dictionary (second argument).
+    Returns -1 if the event is not available.
+    """
+    return disc_lookup.get((event.run, event.lumi, event.event), -1)
+
+def fill_discriminator(oldTree, newTree, disc_lookup, s,
+        start=0, end=-1):
+    """
+    Args:
+        oldTree: tree from the input ROOT file
+        newTree: clone of the input tree
+        disc_lookup: dictionary of the form {(run, lumi, event):disc, ...}
+        s: struct linked to discriminator branch in newTree
+        fill_all: if True, will fill events even if no BDT value available
+    Returns: None
+
+    Fills newTree with all events from oldTree, with discriminator
+    values filled from the lookup table where present.
+    """
+    if end < 0:
+        end = oldTree.GetEntries()
+    for i in range(start, end):
+        oldTree.GetEntry(i)
+        if i % 1000 == 0:
+            print "Processing event {}".format(i)
+        s.disc = get_disc_or_minusone(oldTree, disc_lookup)
+        if s.disc > -1:
+            newTree.Fill()
+
+
+if __name__ == '__main__':
+    root.gROOT.SetBatch()
+    root.gROOT.Reset()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('in_name')
+    parser.add_argument('--chunk-size', type=int, default=10000,
+            help='Number of input events to read per job')
+    parser.add_argument('--chunk', type=int, default=0,
+            help='Which chunk # to run on (0-indexed)')
+    args = parser.parse_args()
+
+    # Get tree for output
+    tree_name = 'DelayedPhoton'
+    bkgFile = root.TFile(args.in_name)
+    bkgTree = bkgFile.Get(tree_name)
+
+    # Define variables to be used for BDT
+    variables = ['pho1ecalPFClusterIso', 'pho1trkSumPtHollowConeDR03',
+            'pho1PFsumNeutralHadronEt', 'pho1SigmaIetaIeta', 'pho1Smajor',
+            'pho1Sminor','pho1R9'] 
+    
+    # To write out the events with GBM discriminator appended, 
+    # we identify the test events by run/lumi/event number.
+    id_variables = ['run', 'lumi', 'event']
+
+    # Get ROOT file into pandas
+    df_iter = rp.read_root(args.in_name, tree_name,
+            columns=variables+id_variables, chunksize=args.chunk_size)
+    # root_pandas seems to be set up to read a ROOT file sequentially.
+    # Therefore we have to cycle through the file to find the 
+    # part we want.
+    for i, df in enumerate(df_iter):
+        if i == args.chunk:
+            break
+
+    # Get min and max entries for this job
+    num_entries = bkgTree.GetEntries()
+    start_entry = args.chunk * args.chunk_size
+    end_entry = min(num_entries,
+            (args.chunk + 1) * args.chunk_size)
+    if num_entries % args.chunk_size == 0:
+        num_chunks = num_entries / args.chunk_size
+    else:
+        num_chunks = (num_entries / args.chunk_size) + 1
+    print "Input ROOT file has {} entries.".format(num_entries)
+    print "Will process chunk {} (entries {} - {})".format(
+            args.chunk, start_entry, end_entry)
+
+    # Get index of each variable in the DataFrame
+    var_indices = [df.columns.get_loc(v) for v in variables] 
+    id_var_indices = [df.columns.get_loc(v) for v in id_variables]
+
+    # Get numpy array from pandas data frame
+    x = df.values
+    x_reduced = x[:,var_indices] # use this for BDT
+    x_index = x[:,id_var_indices] # use this to give each event an ID
+
+    # Read in model saved from previous running of BDT
+    infile = open("model.pkl","rb")
+    model = pickle.load(infile)
+
+    # make prediction for input data
+    y_pred = model.predict_proba(x_reduced)[:, 1]
+
+    # Create a lookup table for discriminator value by event number
+    disc_lookup = {}
+    for disc_val, run, lumi, event in zip(
+            y_pred, x_index[:,0], 
+            x_index[:,1], x_index[:,2]):
+        disc_lookup[(run, lumi, event)] = disc_val
+
+    # Write to a new ROOT file in the local directory
+    out_bkg_name = os.path.basename(args.in_name).replace(
+            '.root', '_BDT_{}of{}.root'.format(
+                args.chunk, num_chunks))
+    out_bkg = root.TFile(out_bkg_name, 'RECREATE')
+    out_bkg_tree = bkgTree.CloneTree(0)
+
+    # This is the boilerplate code for writing something
+    # to a ROOT tree from Python.
+    root.gROOT.ProcessLine("struct MyStruct{float disc;};")
+    from ROOT import MyStruct
+    s = MyStruct()
+    disc_branch_bkg = out_bkg_tree.Branch(
+            'disc', root.AddressOf(s, 'disc'), 'disc/F');
+
+    print "Writing new ROOT background file with discriminator appended"
+    fill_discriminator(bkgTree, out_bkg_tree, disc_lookup, s,
+            start=start_entry, end=end_entry)
+
+    # Cristian's code uses GetCurrentFile() for this part.
+    # I will do that too just in case (paranoia).
+    out_bkg.cd()
+    out_bkg_tree.GetCurrentFile().Write()
+    out_bkg_tree.GetCurrentFile().Close()
--- displaced_photon_classifier/README.md
+++ displaced_photon_classifier/README.md
@@ -19,11 +19,12 @@ cd CMSSW_9_3_3/src/
 cmsenv
 cd -
 python PhotonTraining.py
-python PhotonTraining_onGJets_rootupdated.py
 ```
-
-If want to run on another type of background, then the file `PhotonTraining_onGJets_rootupdated.py` should be modified to input a different file (line 59). If the origional model needs to be modified, this would be done from `PhotonTraining.py` on line 52 or 58 (signal and background files).
-
 This is currently set to run on the files from `/afs/cern.ch/work/g/gkopp/public/ThesisROOTfiles/`. These files are from the Monte Carlo that Zhicai made, and I skimmed the ROOT files on pho1Pt > 0 and for in barrel photons.
-
 *Note the discriminator values are set to -1 for the events that have been used in the training set* - and these events should never be used again since they would make it biased.
+
+This will create the initial BDT and produce ROOT files for the QCD background and the GMSB signal.  To run the BDT on a different input ROOT file and save the score for each event, use the `EvalBDT.py` script.  This takes as arguments the path to the input ROOT file, the chunk size (number of events to process in one job), and a number indicating which chunk to process.  Example:
+```
+python EvalBDT.py --chunk-size 5000 --chunk 2 /eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/withcut/GMSB_L150TeV_Ctau20000cm_13TeV-pythia8.root
+```
+WARNING: do not run `EvalBDT.py` on any ROOT files containing events used in the original BDT training.  Reusing the training events will give a biased distribution of BDT discriminator values.
